042380a876324fe4036451aae755f609d95c0ae0
Aaron <nbaaron13@yahoo.com>
2020-05-03 12:41:47 -0500

    finished

--- etchsketch.js
+++ etchsketch.js
@@ -1,54 +1,47 @@
 const container = document.querySelector('#container');
-let content = [];
+let cell = [];
 
 let number = 16;
 let numberOfPixels = number*number; 
 
-let buttonClear;
-
-let sizeOfCanvas = 320;
+let sizeOfCanvas = 500;
 let sizeOfPixels = sizeOfCanvas / number;
 
-buttonClear = document.getElementById('clear');
-buttonClear.addEventListener('click', clearGrid);
-buttonClear.addEventListener('click', changeGridSize);
-buttonClear.addEventListener('click', setGridSize);
+let resetButton;
+
+resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', changeGridSize);
+resetButton.addEventListener('click', setGridSize);
 
 setGridSize();
 function setGridSize () {
     sizeOfPixels = sizeOfCanvas / number;
     for (let i = 0; i < numberOfPixels; i++) {
 
-        content[i] = document.createElement('div');
-        content[i].classList.add('content');
+        cell[i] = document.createElement('div');
+        cell[i].classList.add('cell');
 
-        content[i].style.width = sizeOfPixels + 'px';
-        content[i].style.height = sizeOfPixels + 'px';
+        cell[i].style.width = sizeOfPixels + 'px';
+        cell[i].style.height = sizeOfPixels + 'px';
 
-        content[i].addEventListener('mouseover', rainbowColor);
-        container.appendChild(content[i]);
+        cell[i].addEventListener('mouseover', blackColor);
+        container.appendChild(cell[i]);
 }}
-
-function blackColor () {
-    this.style.backgroundColor = 'black';
-}
-
-function clearGrid () {
-    for (let i = 0; i < numberOfPixels; i++) {
-        content[i].style.backgroundColor = none;
-    }
-};
 
 function changeGridSize () {
     for (let i = 0; i < numberOfPixels; i++) {
-        content[i].remove();
+        cell[i].remove();
     }do {
         number = prompt('Select canvas size (between 1 and 100): ');
     } while (number < 1 || number > 100);
     numberOfPixels = number*number;
 }
 
-function rainbowColor () {
+function blackColor() {
+  this.style.backgroundColor = 'black';
+}
+
+function rainbowColor() {
   let x = Math.floor(Math.random() * 256);
   let y = Math.floor(Math.random() * 256);
   let z = Math.floor(Math.random() * 256);
